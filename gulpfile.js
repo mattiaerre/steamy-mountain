e08e5c764b6119948f1b7e7cad593003d790adaa
mattia richetto <mattia.richetto@gmail.com>
2016-03-29 14:33:36 +0100

    add react

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 var gulp = require('gulp');
 var LiveSerer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 gulp.task('live-server', function() {
   var server = new LiveSerer('bin/www');
   server.start();
 });
 
-gulp.task('serve', ['live-server'], function(){
+gulp.task('bundle', function() {
+  return browserify({
+    entries: 'public/app/main.jsx',
+    debug: true
+  })
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('serve', ['live-server'], function() {
   browserSync.init(null, {
     proxy: 'http://localhost:7777',
     port: 9001
   });
 });
+
